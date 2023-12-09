62abcc90dcb048751f5f087589f81d8a1d48767a
Pilmo <pilmokim99@gmail.com>
2023-12-09 16:54:56 +0900

    use sigalrm instead of sleep

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,23 @@ use clap::Parser;
 use crate::cli::Args;
 use crate::executor::{execute_command, execute_failure_script, FailureScriptEnv};
 // use libc;
-
+use nix::sys::signal::*;
 use nix::sys::signal::{self, Signal};
-use std::thread;
-use std::time::{Duration, SystemTime, UNIX_EPOCH};
+
+use std::time::{SystemTime, UNIX_EPOCH};
 // use nix::sys::signal::{Signal, self};
 use nix::unistd::Pid;
 use nix::errno::Errno;
 use nix::Result;
+use nix::unistd::alarm;
+use std::sync::atomic::{AtomicBool, Ordering};
 
+static ALARM_RECEIVED: AtomicBool = AtomicBool::new(false);
+
+extern fn signal_handler(_: nix::libc::c_int) { 
+    println!("Alarm signal received");
+    ALARM_RECEIVED.store(true, Ordering::SeqCst);
+}
 
 fn send_signal(pid: u32, signal_name: &str) -> Result<()> {
     let signal = match signal_name {
@@ -29,57 +37,77 @@ fn send_signal(pid: u32, signal_name: &str) -> Result<()> {
 
 
 fn main() {
+
+    let sa = SigAction::new(
+        SigHandler::Handler(signal_handler),
+        SaFlags::SA_RESTART,
+        SigSet::empty()
+    );
+    unsafe {
+        sigaction(Signal::SIGALRM, &sa).expect("Failed to set signal handler");
+    }
     let args = Args::parse();
     let interval = args.interval;
     // let mut failure_count = 0;
     // let mut last_recovery_time = Instant::now();
-
+    alarm::set(interval as u32);
+    let mut old_mask = SigSet::empty();
+    let mut mask = SigSet::empty();
+    mask.add(Signal::SIGALRM);
     loop {
-        let start = SystemTime::now();
-        let (output, fail_pid) = execute_command(&args);
-        let unix_time = start.duration_since(UNIX_EPOCH).unwrap().as_secs();
-        let exit_code = output.status.code().unwrap_or(1);
+        // Block SIGALRM
+        let _ = sigprocmask(SigmaskHow::SIG_BLOCK, Some(&mask), Some(&mut old_mask));
+        if ALARM_RECEIVED.load(Ordering::SeqCst) {
+            ALARM_RECEIVED.store(false, Ordering::SeqCst);
+            alarm::set(interval as u32);
+            let start = SystemTime::now();
+            
+            let (output, fail_pid) = execute_command(&args);
+            let unix_time = start.duration_since(UNIX_EPOCH).unwrap().as_secs();
+            let exit_code = output.status.code().unwrap_or(1);
 
-        // when command failed
-        if !output.status.success() {
-            // failure_count += 1;
-            if args.failure_script.is_some(){
-                println!("failure_script is wrote!!");
-                let failure_env = FailureScriptEnv{
-                    exit_code,
-                    unix_time,
-                    interval,
-                    fail_pid,
-                };
-                execute_failure_script( "./failure.sh", failure_env);
-            }
-
-            if args.pid.is_some(){
-                println!("pid is wrote!!"); 
-                let pid = args.pid.unwrap();
-                let signal = args.signal.as_deref().unwrap_or("HUP");
-                if let Err(e) = send_signal(pid, signal) {
-                    eprintln!("Failed to send signal: {}", e);
-                    return;
+            // when command failed
+            if !output.status.success() {
+                // failure_count += 1;
+                if args.failure_script.is_some(){
+                    println!("failure_script is wrote!!");
+                    let failure_env = FailureScriptEnv{
+                        exit_code,
+                        unix_time,
+                        interval,
+                        fail_pid,
+                    };
+                    execute_failure_script( "./failure.sh", failure_env);
                 }
+
+                if args.pid.is_some(){
+                    println!("pid is wrote!!"); 
+                    let pid = args.pid.unwrap();
+                    let signal = args.signal.as_deref().unwrap_or("HUP");
+                    if let Err(e) = send_signal(pid, signal) {
+                        eprintln!("Failed to send signal: {}", e);
+                        return;
+                    }
+                }
+                } else {
+                    // failure_count = 0;
+
+                }
+
+                let status = match output.status.code() {
+                    Some(0) => String::from("OK"),
+                    Some(code) => format!("Failed: Exit Code {}", code),
+                    None => String::from("Failed: Unknown Error"),
+                };
+
+                println!(
+                    "{}: {}",
+                    start.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+                    status
+                );
             }
-        } else {
-            // failure_count = 0;
-
-        }
-
-        let status = match output.status.code() {
-            Some(0) => String::from("OK"),
-            Some(code) => format!("Failed: Exit Code {}", code),
-            None => String::from("Failed: Unknown Error"),
-        };
-
-        println!(
-            "{}: {}",
-            start.duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
-            status
-        );
-
-        thread::sleep(Duration::from_secs(interval));
+            // Unblock SIGALRM
+            let _ = sigprocmask(SigmaskHow::SIG_UNBLOCK, Some(&mask), None);
+            
     }
 }
